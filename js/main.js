550fa54663220e7ac336f1f43fde7f70034d2031
reo101 <reo101gamer@gmail.com>
2020-05-02 21:03:18 +0300

    Start of autocompletion thingy

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 class Command {
-	constructor(id, command, label, url, prefix = '', suffix = '', isSpecial = false, isShortcut = false) {
+	constructor(id, commands, label, url, prefix = '', suffix = '', isSpecial = false, isShortcut = false, hasCallback = false, callbackURLPrefix = "", callbackURLSufix = "", decodeJSON = () => { }) {
 		this.id = id;
-		this.command = command;
+		this.commands = commands;
 		this.label = label;
 		this.url = url;
 		this.prefix = prefix;
 		this.suffix = suffix;
 		this.isSpecial = isSpecial;
 		this.isShortcut = isShortcut;
+		this.hasCallback = hasCallback;
+		this.callbackScript = "none";
+		this.callbackURLPrefix = callbackURLPrefix;
+		this.callbackURLSufix = callbackURLSufix;
+		this.decodeJSON = decodeJSON;
 	}
 
 	makeUrl(term) {
@@ -30,32 +35,78 @@ class Command {
 				return '# Search with ' + this.label;
 		}
 	}
+
+	getCallbackdata(value) {
+		if (!this.hasCallback) {
+			return null;
+		}
+
+		var xhr = new XMLHttpRequest();
+
+		var url = "https://cors-anywhere.herokuapp.com/" + this.callbackURLPrefix + value + this.callbackURLSufix;
+
+		xhr.open("GET", url);
+		xhr.onload = xhr.onerror = () => {
+			let rawValue = xhr.responseText;
+			// console.log(rawValue);
+			let array = this.decodeJSON(rawValue);
+
+			let list = "";
+			array.forEach(element => {
+				list += "<option>" + element + "</option>";
+			});
+			document.getElementById("options").innerHTML = list;
+
+
+			// autocomplete(search, this.decodeJSON(rawValue));
+		};
+
+		if (/^POST/i.test("GET")) {
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		}
+		xhr.send();
+	}
 }
 
 commands = [
-	new Command(0, '', 'DuckDuckGo', 'https://duckduckgo.com/', '?q='),
-	new Command(1, '/d', 'DuckDuckGo', 'https://duckduckgo.com/', '?q='),
-	new Command(2, '/g', 'Google', 'https://google.com/', 'search?q='),
-	new Command(3, '/t', 'Torrents', 'https://torrentz2.eu/', 'search?f='),
-	new Command(4, '/x', '1337x', 'https://1337x.to/', 'search/', '/1/'),
-	new Command(5, '/y', 'YouTube', 'https://www.youtube.com/', 'results?search_query='),
-	new Command(6, '/yt', 'YouTube', 'https://www.youtube.com/', 'results?search_query='),
-	new Command(7, 'r/', 'Reddit', 'https://www.reddit.com/', 'r/', '/', true),
-	new Command(8, '/z', 'Zamunda', 'https://www.zamunda.net/', 'bananas?c42=1&c25=1&c35=1&c20=1&c19=1&c5=1&c24=1&c7=1&c33=1&c4=1&c21=1&c1=1&c22=1&search=', '&gotonext=1&incldead=&field=name'),
-	new Command(9, '/zf', 'Zamunda Films', 'https://www.zamunda.net/', 'bananas?c42=1&c25=1&c35=1&c20=1&c19=1&c5=1&c24=1&c7=1&c33=1&search=', '&gotonext=1&incldead=&field=name'),
-	new Command(10, '/zg', 'Zamunda Games', 'https://www.zamunda.net/', 'bananas?c4=1&c21=1&c1=1&c22=1&search=', '&gotonext=1&incldead=&field=name'),
-	new Command(11, 'shk', 'Shkolo', 'https://app.shkolo.bg/dashboard', '', '', true, true),
-	new Command(12, 'spme', 'Spotify stats', 'https://spotify.me', '', '', true, true),
-	new Command(13, '/s', 'Stack Overflow', 'https://www.stackoverflow.com/', 'search?q='),
-	new Command(14, 'rs', 'RipSave', 'https://ripsave.com/ ', '', '', true, true),
-	new Command(15, 'xpi', 'Firefox XPI', 'http://reo.free.bg/xpi/', '', '', true, true),
-	new Command(16, 'fmi', 'FMI Exams', 'https://www.fmi.uni-sofia.bg/bg/sample-exams-view', '', '', true, true),
+	new Command(0, [''], 'DuckDuckGo', 'https://duckduckgo.com/', '?q='),
+	new Command(1, ['/d'], 'DuckDuckGo', 'https://duckduckgo.com/', '?q='),
+	new Command(2, ['/g'], 'Google', 'https://google.com/', 'search?q=', false, false, false, true,
+		"https://www.google.com/complete/search?q=",
+		"&cp=10&client=psy-ab&xssi=t&gs_ri=gws-wiz&hl=en-BG&authuser=0&psi=A4KtXrKPGuidlwSAyp2YAg.1588429313922",
+		value => {
+			// console.log(value.substring(5));
+			value = value.substring(5);//.split[")]}'"];
+			let array = eval(value);
+			array = array[0].map(element => element[0]);
+			console.log(array);
+			return array;
+			// autocomplete(search, array);
+		}),
+	new Command(3, ['/t'], 'Torrents', 'https://torrentz2.eu/', 'search?f='),
+	new Command(4, ['/x'], '1337x', 'https://1337x.to/', 'search/', '/1/'),
+	new Command(5, ['/y', '/yt'], 'YouTube', 'https://www.youtube.com/', 'results?search_query=', false, false, false, true, "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=", ""),
+	new Command(6, ['r/'], 'Reddit', 'https://www.reddit.com/', 'r/', '/', true),
+	new Command(7, ['/z'], 'Zamunda', 'https://www.zamunda.net/', 'bananas?c42=1&c25=1&c35=1&c20=1&c19=1&c5=1&c24=1&c7=1&c33=1&c4=1&c21=1&c1=1&c22=1&search=', '&gotonext=1&incldead=&field=name'),
+	new Command(8, ['/zf'], 'Zamunda Films', 'https://www.zamunda.net/', 'bananas?c42=1&c25=1&c35=1&c20=1&c19=1&c5=1&c24=1&c7=1&c33=1&search=', '&gotonext=1&incldead=&field=name'),
+	new Command(9, ['/zg'], 'Zamunda Games', 'https://www.zamunda.net/', 'bananas?c4=1&c21=1&c1=1&c22=1&search=', '&gotonext=1&incldead=&field=name'),
+	new Command(10, ['shk'], 'Shkolo', 'https://app.shkolo.bg/dashboard', '', '', true, true),
+	new Command(11, ['spme'], 'Spotify stats', 'https://spotify.me', '', '', true, true),
+	new Command(12, ['/s'], 'Stack Overflow', 'https://www.stackoverflow.com/', 'search?q='),
+	new Command(13, ['rs'], 'RipSave', 'https://ripsave.com/ ', '', '', true, true),
+	new Command(14, ['xpi'], 'Firefox XPI', 'http://reo.free.bg/xpi/', '', '', true, true),
+	new Command(15, ['fmi'], 'FMI Exams', 'https://www.fmi.uni-sofia.bg/bg/sample-exams-view', '', '', true, true),
 ];
 
 var command = commands[0];
 
 search.addEventListener('keyup', (e) => {
 	var value = search.value;
+	if (command.hasCallback){
+		console.log(value);
+		command.getCallbackdata(value);
+	}
+
 	// if (e.keyCode == 13 || e.which == 13) {
 	// 	if (value.indexOf('.') > 0) {
 	// 		// is this still needed?  smart
@@ -65,14 +116,16 @@ search.addEventListener('keyup', (e) => {
 	if (value[0] == '/' && value[value.length - 1] == ' ') {
 		value = value.trim();
 		for (var i = 0; i < commands.length; i++) {
-			if (value == commands[i].command) {
-				command = commands[i];
-				hideHelp();
-				icon.className = command.icon;
-				search.value = '';
-				search.setAttribute('name', command.prefix);
-				search.setAttribute('placeholder', command.makePlaceholder());
-				break;
+			for (var j = 0; j < commands[i].commands.length; j++) {
+				if (value == commands[i].commands[j]) {
+					command = commands[i];
+					hideHelp();
+					icon.className = command.icon;
+					search.value = '';
+					search.setAttribute('name', command.prefix);
+					search.setAttribute('placeholder', command.makePlaceholder());
+					break;
+				}
 			}
 		}
 	} else if (
@@ -203,6 +256,6 @@ var isBackspace = (val) => {
 	return bool;
 }
 
-transitionsInit();
+// transitionsInit();
 // logo.style.transform = "scale(0.0);";
 // document.getElementsByTagName("canvas")[1].style="max-height: 150px";
